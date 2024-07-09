8cd4d9fb1c656c77961e59ea83bcaf58d38d130e
Luis Henrique <luis@prover.net.br>
2024-07-09 19:14:10 -0300

    feat: static page with revalidate

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,36 +3,65 @@ import styles from "@/styles/home.module.css";
 import Image from 'next/image'
 
 import heroImg from '../../public/assets/hero.png'
+import { GetStaticProps } from "next";
 
-export default function Home() {
+import { db } from "@/services/firebaseConnection";
+
+import {
+  collection,
+  getDocs,
+} from 'firebase/firestore'
+
+interface HomeProps {
+  posts: number,
+  comments: number,
+}
+
+export default function Home({ posts, comments }: HomeProps) {
   return (
-   <div className={styles.container}>
+    <div className={styles.container}>
       <Head>
         <title>Tarefas+ | Organize suas tarefas de forma fácil</title>
-    </Head>
+      </Head>
 
-    <main className={styles.main}>
-      <div className={styles.logoContent}>
-        <Image 
-          className={styles.hero}
-          alt="Logo Tarefas+"
-          src={heroImg}
-          priority
-        />
-      </div>
-      <h1 className={styles.title}>
-        Sistema feito para você organizar <br />
-        seus estudos e tarefas
-      </h1>
-      <div className={styles.infoContent}>
-        <section className={styles.box}>
-          <span>+12 posts</span>
-        </section>
-        <section className={styles.box}>
-          <span>+90 comentários</span>
-        </section>
-      </div>
-    </main>
-   </div>
+      <main className={styles.main}>
+        <div className={styles.logoContent}>
+          <Image
+            className={styles.hero}
+            alt="Logo Tarefas+"
+            src={heroImg}
+            priority
+          />
+        </div>
+        <h1 className={styles.title}>
+          Sistema feito para você organizar <br />
+          seus estudos e tarefas
+        </h1>
+        <div className={styles.infoContent}>
+          <section className={styles.box}>
+            <span>+{posts} posts</span>
+          </section>
+          <section className={styles.box}>
+            <span>+{comments} comentários</span>
+          </section>
+        </div>
+      </main>
+    </div>
   );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+
+  const commentRef = collection(db, "comments")
+  const postRef = collection(db, "tarefas")
+
+  const commentSnapshot = await getDocs(commentRef)
+  const postSnapshot = await getDocs(postRef)
+  return {
+    props: {
+      posts: postSnapshot.size || 0,
+      comments: commentSnapshot.size || 0
+    },
+    revalidate: 60
+  }
 }
